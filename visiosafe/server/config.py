659cde254e3b46bb9f6e61662267cb1c0fe12079
nourimen1 <nouriimendarragi@gmail.com>
2025-04-02 01:48:57 +0100

    0

--- visiosafe/server/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'votre-cle-secrete-ici'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///visiosafe.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_TOKEN_EXPIRES = 3600  # 1 heure
